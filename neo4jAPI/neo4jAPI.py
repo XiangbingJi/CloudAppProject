78c046c93d998db9d196411e91432d50e46f9c11
sky00high <tianyu0726@gmail.com>
2016-11-09 23:02:36 -0500

    add initial file

---
+++ neo4jAPI/neo4jAPI.py
@@ -0,0 +1,15 @@
+#from neo4jrestclient.client import GraphDatabase
+def lambda_handler(event, context):
+
+    return 'Hello from Lambda'
+
+'''
+
+def lambda_handler(event, context):
+    gdb = GraphDatabase("http://hobby-aoggfappojekgbkeiookiiol.dbs.graphenedb.com:24789/db/data/",
+                    username="USPNeo4j",
+                    password="1ShigqV38xWETt45T2AG")
+                    
+    alice = gdb.nodes.create(name="Alice", age=30)
+    return 'Hello from Lambda'
+ '''
