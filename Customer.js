195c2c272aa6edf60abe91a073719f0dfaa0a8d8
俞歆哲 <yuxinzhe@dyn-160-39-172-73.dyn.columbia.edu>
2016-12-23 10:00:46 -0500

    add sns part

--- Customer.js
+++ Customer.js
@@ -3,6 +3,9 @@
 console.log('Loading Customer.js...');
 const doc = require('dynamodb-doc');
 const dynamo = new doc.DynamoDB();
+var aws = require('aws-sdk');  
+var sns = new aws.SNS();
+var topic = "arn:aws:sns:us-east-1:498679776130:slackLambda";
 
 exports.handler = function(event, context, callback) {
     console.log('handler event: ' + JSON.stringify(event));
@@ -32,8 +35,7 @@ exports.handler = function(event, context, callback) {
             break;
         default:
             var err = new Error('405 Unrecognized operation');
-            err.name = 'Unrecognized operation "${event.operation}"';
-            // callback(err, null);
+            err.name = "Unrecognized operation '${event.operation}'";
             putResult(event.res_key, err, callback);
     }
 };
@@ -43,6 +45,7 @@ function putResult(key, res, cb) {
         TableName: "Result",
         Item: { "key": key, "result": JSON.stringify(res) }
     };
+            console.log('putResult data: ' + JSON.stringify(params));
 
     dynamo.putItem(params, function(err, data) {
         if (err) {
@@ -133,7 +136,7 @@ function refineCustomerData(data, req) {
             };
         }
         if (item.email) {
-            item.self = {
+            item['self'] = {
                 "href": req.proto + "://" + req.host + "/" + req.stage + "/customer/" + item.email
             };
         }
@@ -153,24 +156,20 @@ function getCustomer(event, callback) {
 
     if(err) {
         console.log('validateEmail() returns err: ' + JSON.stringify(err));
-        // callback(err, null);
         putResult(event.res_key, err, callback);
     } else {
         dynamo.getItem(params, function (err, data) {
             if (err) {
                 console.log('getCustomer err: ' + JSON.stringify(err));
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
             } else if (Object.keys(data).length === 0) {
                 err = new Error('404 Resource not found');
                 err.name = 'Item is not found in the table, cannot read!';
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
            } else {
                 console.log('getCustomer success, data: ' + JSON.stringify(data));
                 // Only come with request when invoked by API gateway
                 if (event.request) refineCustomerData([data.Item], event.request);
-                // callback(null, data);
                 putResult(event.res_key, data.Item, callback);
             }
         });
@@ -192,7 +191,6 @@ function createCustomer(event, callback) {
         console.log('400 Invalid input, newCustomer does not have enough attributes');
         err = new Error('400 invalid parameter');
         err.name = 'newCustomer does not have enough attributes';
-        // callback(err, null);
         putResult(event.res_key, err, callback);
     }
     else
@@ -201,7 +199,6 @@ function createCustomer(event, callback) {
 
         if (err) {
             console.log('Invalid Input: ' + JSON.stringify(err));
-            // callback(err, null);
             putResult(event.res_key, err, callback);
         } else {
             dynamo.putItem(params, function(err, data) {
@@ -211,12 +208,32 @@ function createCustomer(event, callback) {
                         err.name = 'Creating Customer already exists in the table';
                     }
                     console.log('createCustomer err: ' + JSON.stringify(err));
-                    // callback(err, null);
+                    
                     putResult(event.res_key, err, callback);
+                    
                 } else {
                     console.log('createCustomer success, data: ' + JSON.stringify(data));
-                    // callback(null, data);
-                    putResult(event.res_key, { email: params.Item.email }, callback);
+                    var pub_event = {
+                        "operation": event.operation + " customer",
+                        "email": event.item.email,
+                        "name": event.item.firstname + " " + event.item.lastname,
+                        "phone": event.item.phone_num,
+                        "address": event.item.address
+                    };
+                    sns.publish({
+                        Message: JSON.stringify(pub_event),
+                        TopicArn: topic
+                    }, function(err, data) {
+                        if (err) {
+                            console.log('publish to sns error: ' + err);
+                            putResult(event.res_key, { email: params.Item.email }, callback);
+                        } else {
+                            console.log('publish to sns success');
+                            putResult(event.res_key, { email: params.Item.email }, callback);
+                        }
+                    });
+                    //console.log('go to put result');
+                    //putResult(event.res_key, { email: params.Item.email }, callback);
                 }
             });
         }
@@ -261,7 +278,6 @@ function updateCustomer(event, callback) {
     var err = validateCustomer(event.updates);
     if (err) {
         console.log('validateEmail() returns err: ' + JSON.stringify(err));
-        // callback(err, null);
         putResult(event.res_key, err, callback);
     } else {
         params = updateExpression(event.updates, params);
@@ -272,13 +288,11 @@ function updateCustomer(event, callback) {
                     err.name = 'updating Customer not exists in the table.';
                 }
                 console.log('updateCustomer err: ' + JSON.stringify(err));
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
                 // TODO check errtype == 'ConditionalCheckFailedException'
             } else {
                 console.log('updateCustomer success, data: ' + JSON.stringify(data));
-                // callback(null, data);
-                putResult(event.res_key, data, callback);
+                putResult(event.res_key, params, callback);
             }
         });
     }
@@ -298,8 +312,7 @@ function deleteCustomer(event, callback) {
 
     if(errEmail) {
         console.log('validateEmail() returns err: ' + JSON.stringify(errEmail));
-        // callback(errEmail, null);
-        putResult(event.res_key, err, callback);
+        putResult(event.res_key, errEmail, callback);
     } else {
         dynamo.deleteItem(params, function(err, data) {
             if (err) {
@@ -308,11 +321,9 @@ function deleteCustomer(event, callback) {
                     err.name = 'deleting Customer not exists in the table.';
                 }
                 console.log('deleteCustomer err: ' + JSON.stringify(err));
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
             } else {
                 console.log('deleteCustomer complete, data: ' + JSON.stringify(data));
-                // callback(null, data);
                 putResult(event.res_key, {"result ": event.email + " is delete"}, callback);
             }
         });
@@ -324,26 +335,23 @@ function getCustomerAddress(event, callback) {
         TableName: event.tableName,
         Key: {'email': event.email},
     };
-    
+
     console.log('In deleteCustomer, params is: ' + JSON.stringify(params));
 
     var errEmail = validateEmail(params.Key.email);
 
     if(errEmail) {
         console.log('validateEmail() returns err: ' + JSON.stringify(errEmail));
-        // callback(errEmail, null);
-        putResult(event.res_key, err, callback);
+        putResult(event.res_key, errEmail, callback);
     } else {
         var addressRef = null;
         dynamo.getItem(params, function (err, data) {
             if (err) {
                 console.log('getCustomer err: ' + JSON.stringify(err));
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
             } else if (Object.keys(data).length === 0) {
                 err = new Error('404 Resource not found');
                 err.name = 'Item is not found in the table, cannot read!';
-                // callback(err, null);
                 putResult(event.res_key, err, callback);
            } else {
                 console.log('getCustomer success, data: ' + JSON.stringify(data));
@@ -353,16 +361,13 @@ function getCustomerAddress(event, callback) {
                 dynamo.getItem(params, function(err, data) {
                     if (err) {
                         console.log('get customer address err: ' + JSON.stringify(err));
-                        // callback(err, null);
                         putResult(event.res_key, err, callback);
                     } else if (Object.keys(data).length === 0) {
                         err = new Error('404 Resource not found');
                         err.name = 'Item is not found in the table, cannot read!';
-                        // callback(err, null);
                         putResult(event.res_key, err, callback);
                     } else {
-                        console.log('getCustomer success, data: ' + JSON.stringify(data));
-                        // callback(null, data);
+                        console.log('getCustomer address success, data: ' + JSON.stringify(data));
                         putResult(event.res_key, data.Item, callback);
                     }
                 });
